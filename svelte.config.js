607a9a9c14eed72693db56e96d2c538b96c46275
Vincent Liao <vincent.liao@protonmail.com>
2022-08-29 20:31:22 +0700

    change to edge because why not

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,7 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({ edge: true })
 	}
 };
 
